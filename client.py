a71922c11222bba8b26e886604613e0d933e8213
NickyGund <neg62@scarletmail.rutgers.edu>
2020-02-25 20:48:52 -0500

    update?
    slight changes, added some comments to organize thoughts

--- client.py
+++ client.py
@@ -7,6 +7,7 @@ import random
 import socket
 
 def client():
+    #This populates a list with the addresses in the PROJI-HNS.txt
     file = open("PROJI-HNS.txt", 'r'
     myURLlist = []
     for line in file:
@@ -14,17 +15,32 @@ def client():
     try:
         rs = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         print("[C]: Root DNS socket created")
+        port = #user input#
+        localhost_addr = socket.gethostbyname(socket.gethostname())
 
+
+
+
+        except socket.error as err:
+        print('socket open error: {} \n'.format(err))
+        exit()
+#I was thinking of having 2 tries? Both transmitting strings across sockets to said dns server
+    try:
         ts = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         print("[C]: Top Level DNS socket created")
+        port = #user input- rs listening port#
 
-    except socket.error as err:
+
+
+
+        except socket.error as err:
         print('socket open error: {} \n'.format(err))
         exit()
 
+
     # Define the port on which you want to connect to the server
-    port = 50007
-    localhost_addr = socket.gethostbyname(socket.gethostname())
+        port = 6007
+        localhost_addr = socket.gethostbyname(socket.gethostname())
 
     #connect to the server on the rs
 
--- rs.py
+++ rs.py
@@ -23,13 +23,20 @@ def server():
     ss.bind(server_binding)
     ss.listen(1)
     host = socket.gethostname()
+
+
     print("[S]: Server host name is {}".format(host))
     localhost_ip = (socket.gethostbyname(host))
     print("[S]: Server IP address is {}".format(localhost_ip))
     csockid, addr = ss.accept()
     print ("[S]: Got a connection request from a client at {}".format(addr))
 
-    # send a intro message to the client.
+    # send a intro message to the client, Here we want to send them the string
+    #we were looking for
+
+    # FOR Loop here where we compare the addresses given in the file
+    # to the addresses in Proj-I DNSRS.txt
+    
     msg = "Welcome to CS 352!"
     csockid.send(msg.encode('utf-8'))
 
