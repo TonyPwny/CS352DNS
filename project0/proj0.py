0b292f0072563f6cbd9d796f839b6f632bef9c7d
Anthony Tiongson <anthonytiongson@me.com>
2020-02-25 22:58:27 -0500

    just adding folders of original files for reference puposes

---
+++ project0/proj0.py
@@ -0,0 +1,66 @@
+import threading
+import time
+import random
+
+import socket
+
+def server():
+    try:
+        ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print("[S]: Server socket created")
+    except socket.error as err:
+        print('socket open error: {}\n'.format(err))
+        exit()
+
+    server_binding = ('', 50007)
+    ss.bind(server_binding)
+    ss.listen(1)
+    host = socket.gethostname()
+    print("[S]: Server host name is {}".format(host))
+    localhost_ip = (socket.gethostbyname(host))
+    print("[S]: Server IP address is {}".format(localhost_ip))
+    csockid, addr = ss.accept()
+    print ("[S]: Got a connection request from a client at {}".format(addr))
+
+    # send a intro message to the client.  
+    msg = "Welcome to CS 352!"
+    csockid.send(msg.encode('utf-8'))
+
+    # Close the server socket
+    ss.close()
+    exit()
+
+def client():
+    try:
+        cs = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print("[C]: Client socket created")
+    except socket.error as err:
+        print('socket open error: {} \n'.format(err))
+        exit()
+        
+    # Define the port on which you want to connect to the server
+    port = 50007
+    localhost_addr = socket.gethostbyname(socket.gethostname())
+
+    # connect to the server on local machine
+    server_binding = (localhost_addr, port)
+    cs.connect(server_binding)
+
+    # Receive data from the server
+    data_from_server=cs.recv(100)
+    print("[C]: Data received from server: {}".format(data_from_server.decode('utf-8')))
+
+    # close the client socket
+    cs.close()
+    exit()
+
+if __name__ == "__main__":
+    t1 = threading.Thread(name='server', target=server)
+    t1.start()
+
+    time.sleep(random.random() * 5)
+    t2 = threading.Thread(name='client', target=client)
+    t2.start()
+
+    time.sleep(5)
+    print("Done.")
